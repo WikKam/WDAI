5d706100cf44a8e9b7f618459ae8124ea7676074
WikKam <kamykikamienie@gmail.com>
2020-04-17 22:10:07 +0200

    cleanup

--- src/app/services/course.service.ts
+++ src/app/services/course.service.ts
@@ -1,6 +1,5 @@
 import { Injectable, EventEmitter, Output } from '@angular/core';
 import {Course} from '../models/Course';
-import {getExampleCourses, addCourse, deleteCourse, getCourse, changeRating} from '../models/ExampleCourses';
 import { Observable, of } from 'rxjs';
 @Injectable({
   providedIn: 'root'
@@ -9,21 +8,6 @@ export class CourseService {
   courses: Course[];
   @Output() change: EventEmitter<any> = new EventEmitter();
   constructor() { }
-  getCourses():Observable<Course[]>{
-    return of(getExampleCourses());
-  }
-  addCourse(course:Course):void{
-      addCourse(course);
-  }
-  deleteCourse(course:Course):void{
-    deleteCourse(course);
-  }
-  getCourseWithId(id:any):Observable<Course>{
-   return of(getCourse(id));
-  }
-  changeRating(id:number, rating:number):void{
-    changeRating(id,rating);
-  }
   updateCourses(courses: Course[]){
     this.courses = courses;
   }
